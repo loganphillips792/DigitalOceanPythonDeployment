8773bd62da45eb3e3e7c1a28393f4795825385ec
loganphillips792 <loganphillips792@gmail.com>
2022-10-29 14:47:36 -0400

    increase pylint score

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,20 +1,28 @@
+"""
+This is a doc string to pass linting
+"""
+
+import logging
 from flask import Flask
 from flask_cors import CORS
-import logging
-import sys
-
 
 # https://stackoverflow.com/questions/26578733/why-is-flask-application-not-creating-any-logs-when-hosted-by-gunicorn
 
 
-# stream handler needed to get logging to write to standard output so we can see it in Docker logs (if we aren't using Gunicorn)
-# logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s',level=logging.INFO, handlers=[logging.StreamHandler(stream=sys.stdout)])
+# stream handler needed to get logging to write to standard output
+# so we can see it in Docker logs (if we aren't using Gunicorn)
+# logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s'
+# ,level=logging.INFO, handlers=[logging.StreamHandler(stream=sys.stdout)])
 logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s',level=logging.INFO)
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
 def hello_world():
+    """
+    hello world route
+    """
     logger = logging.getLogger()
     logger.info("Hitting the hello world")
     return {'message': 'Hello, world'}
+    
